14b8c90223b04a8f8e25930baaaa2bd760e68d14
Michael Webster <miketwebster@gmail.com>
2015-02-11 09:18:28 -0500

    st: Use a more up-to-date cogl API (efforts to reduce freezes) - see http://pastebin.com/a1uiMx5j for suspect backtrace.

--- src/st/st-theme-node-drawing.c
+++ src/st/st-theme-node-drawing.c
@@ -68,6 +68,8 @@ elliptical_arc (cairo_t *cr,
 static CoglHandle
 create_corner_material (StCornerSpec *corner)
 {
+  ClutterBackend *backend = clutter_get_default_backend ();
+  CoglContext *ctx = clutter_backend_get_cogl_context (backend);
   CoglHandle texture;
   cairo_t *cr;
   cairo_surface_t *surface;
@@ -165,12 +167,12 @@ create_corner_material (StCornerSpec *corner)
 
   cairo_surface_destroy (surface);
 
-  texture = cogl_texture_new_from_data (size, size,
-                                        COGL_TEXTURE_NONE,
-                                        CLUTTER_CAIRO_FORMAT_ARGB32,
-                                        COGL_PIXEL_FORMAT_ANY,
-                                        rowstride,
-                                        data);
+  texture = COGL_TEXTURE (cogl_texture_2d_new_from_data (ctx, size, size,
+                                                         CLUTTER_CAIRO_FORMAT_ARGB32,
+                                                         rowstride,
+                                                         data,
+                                                         NULL));
+
   g_free (data);
   g_assert (texture != COGL_INVALID_HANDLE);
 
@@ -930,6 +932,8 @@ paint_inset_box_shadow_to_cairo_context (StThemeNode     *node,
 static CoglHandle
 st_theme_node_prerender_background (StThemeNode *node)
 {
+  ClutterBackend *backend = clutter_get_default_backend ();
+  CoglContext *ctx = clutter_backend_get_cogl_context (backend);
   StBorderImage *border_image;
   CoglHandle texture;
   guint radius[4];
@@ -1249,13 +1253,13 @@ st_theme_node_prerender_background (StThemeNode *node)
   if (interior_path != NULL)
     cairo_path_destroy (interior_path);
 
-  texture = cogl_texture_new_from_data (paint_box.x2 - paint_box.x1,
-                                        paint_box.y2 - paint_box.y1,
-                                        COGL_TEXTURE_NONE,
-                                        CLUTTER_CAIRO_FORMAT_ARGB32,
-                                        COGL_PIXEL_FORMAT_ANY,
-                                        rowstride,
-                                        data);
+  texture = COGL_TEXTURE (cogl_texture_2d_new_from_data (ctx,
+                                                         paint_box.x2 - paint_box.x1,
+                                                         paint_box.y2 - paint_box.y1,
+                                                         CLUTTER_CAIRO_FORMAT_ARGB32,
+                                                         rowstride,
+                                                         data,
+                                                         NULL));
 
   cairo_destroy (cr);
   cairo_surface_destroy (surface);
