8177e408985c932f04281e6dbf964e9c860d7893
Michael Webster <miketwebster@gmail.com>
2026-03-21 19:40:04 -0400

    test

--- files/usr/share/cinnamon/applets/power@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/power@cinnamon.org/applet.js
@@ -3,6 +3,7 @@ const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
 const Interfaces = imports.misc.interfaces
 const Lang = imports.lang;
+const LoginManager = imports.misc.loginManager;
 const PowerUtils = imports.misc.powerUtils;
 const St = imports.gi.St;
 const Tooltips = imports.ui.tooltips;
@@ -336,6 +337,12 @@ class CinnamonPowerApplet extends Applet.TextIconApplet {
                 this._devicesChanged();
             }));
         }, null);
+
+        this._loginManager = LoginManager.getLoginManager();
+        this._loginManager.connect('prepare-for-sleep', (aboutToSuspend) => {
+            if (!aboutToSuspend)
+                this._devicesChanged();
+        });
 
         this.set_show_label_in_vertical_panels(false);
     }
